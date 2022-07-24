0e5f5a36a0406968cfffbc291601a59d13621e4f
KuzeyKurt <japarov.s.i20@gmail.com>
2022-07-24 11:58:45 +0300

    после содания store.js и импорта его в main.js сайт пропал, хотя собирается, билдится, компилируется

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
+import { createApp} from 'vue'
 import App from './App.vue'
+import store from './vuex/store'
 import './assets/styles/styles.scss'
 
+App.config.productionTip = false
+ store
 createApp(App).mount('#app')
